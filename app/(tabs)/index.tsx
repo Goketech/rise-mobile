9744266f1a6e022702ae0b018f9befa9815aefce
Modupe Akanni <modupe775@gmail.com>
2025-01-23 17:09:10 +0100

    feat: onboarding flow

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,74 +1,119 @@
-import { Image, StyleSheet, Platform } from 'react-native';
-
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { Image, StyleSheet, Platform, View, Text, Button } from "react-native";
+import { useState } from "react";
 
 export default function HomeScreen() {
+  const [screen, setScreen] = useState(0);
+
+  const getScreenContent = () => {
+    switch (screen) {
+      case 0:
+        return {
+          image: require("@/assets/images/trust.png"),
+          text: "Trusted by millions of people, part of one part",
+          buttonText: "Next",
+        };
+      case 1:
+        return {
+          image: require("@/assets/images/send.png"),
+          text: "Spend money abroad, and track your expense",
+          buttonText: "Next",
+        };
+      case 2:
+        return {
+          image: require("@/assets/images/receive.png"),
+          text: "Receive Money From Anywhere In The World",
+          buttonText: "Get Started",
+        };
+      default:
+        return {};
+    }
+  };
+
+  const { image, text, buttonText = "" } = getScreenContent();
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12'
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <View style={styles.conatiner}>
+      <Image
+        source={image}
+        style={styles.mainIcon}
+      />
+
+<View style={styles.navigation}>
+        <View style={[styles.normal, screen === 0 && styles.active]}></View>
+        <View style={[styles.normal, screen === 1 && styles.active]}></View>
+        <View style={[styles.normal, screen === 2 && styles.active]}></View>
+      </View>
+
+      <View>
+        <Text style={styles.text}>
+          {text}
+        </Text>
+      </View>
+
+      <View style={styles.buutonWrraper}>
+        <View style={styles.button}>
+          <Button
+            onPress={() => setScreen((prev) => (prev + 1) % 3)}
+            title={buttonText}
+            color="#fff"
+            accessibilityLabel="next"
+          />
+        </View>
+      </View>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
+  conatiner: {
+    flex: 1,
+    paddingTop: 200,
+    alignItems: "center",
+    backgroundColor: "#fff",
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  navigation: {
+    marginTop: 88,
+    marginBottom: 24,
+    flexDirection: "row",
+    gap: 6,
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  active: {
+    backgroundColor: "#304FFE",
+    width: 16,
+    height: 8,
+    borderRadius: 19,
+  },
+  normal: {
+    width: 37,
+    height: 8,
+    borderRadius: 19,
+    backgroundColor: "#D0D0D0",
+  },
+  text: {
+    color: "#2A2A2A",
+    fontSize: 34,
+    fontWeight: 600,
+    lineHeight: 41,
+    textAlign: "center",
+    maxWidth: 300,
+  },
+  mainIcon: {
+    height: 260,
+    resizeMode: "contain",
+    borderRadius: 12,
+  },
+  buutonWrraper: {
+    width: "100%",
+  },
+  button: {
+    marginLeft: 17,
+    marginRight: 17,
+    backgroundColor: "#304FFE",
+    borderWidth: 1,
+    borderColor: "transparent",
+    color: "#fff",
+    borderRadius: 50,
+    padding: 10,
+    marginTop: 60,
   },
 });
